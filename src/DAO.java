5b6481e986ca7bde5b0fef02fc70c85f6eaac656
Uma Mahesh <adabala.um@gmail.com>
2024-11-26 20:19:18 +0530

    JDBC Connection

--- src/DAO.java
+++ src/DAO.java
@@ -1,4 +1,40 @@
-package PACKAGE_NAME;
+import java.net.ConnectException;
+import java.sql.*;
+
+
+ // DAO : Data Access Object
+
+// This is used to create Some Specific methods to query the DataBase without disturbing the main methods
 
 public class DAO {
+    public static void main(String[] args) throws Exception {
+        PersonQuery pq = new PersonQuery();
+        Person p1 = pq.getPerson("Umesh");
+        System.out.println(p1.FirstName);
+    }
+}
+
+class PersonQuery{
+    public Person getPerson(String LastName) throws Exception{
+        Person p = new Person();
+        p.LastName = LastName;
+        String Query = "select FirstName from data where LastName=?";
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        Connection cn = DriverManager.getConnection("jdbc:mysql://localhost:3306/JDBCTEST", "root", "@nony1472");
+
+       PreparedStatement psn = cn.prepareStatement(Query);
+       psn.setString(1,LastName);
+        ResultSet rs = psn.executeQuery();
+        rs.next();
+        p.FirstName = rs.getString(1);
+
+
+        return p;
+    }
+}
+
+class Person{
+    String LastName;
+    String FirstName;
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,52 @@
+import java.sql.*;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception{
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        String url = "jdbc:mysql://localhost:3306/JDBCTEST";
+        String Query = "Select FirstName From data where LastName = \"Ganesh\" ";
+        String uname = "root";
+        String pass = "@nony1472";
+        Connection cn = DriverManager.getConnection(url, uname, pass);
+        Statement sn = cn.createStatement();
+
+//        ResultSet res = sn.executeQuery( Query);
+//        res.next();
+//        System.out.println(res.getString("FirstName"));
+
+
+        // Fetch Complete Table
+
+//        Query = "select * from data";
+//
+//        ResultSet res =  sn.executeQuery(Query);
+//        while(res.next()){
+//            System.out.println(res.getString(1)+" "+res.getString(2));
+//        }
+
+
+        // Insert into database
+
+//        Query = "Insert into data values(\"Mallireddy\", \"Geetha\")";
+//
+//        int count = sn.executeUpdate(Query);
+//        System.out.println(count+" rows Effected");
+
+        // Using PreparedStatement for Dynamic Values
+
+        Query = "Insert into data values (?,?)";
+        String LastName = "Manoj";
+        String FirstName = "Posipo";
+        PreparedStatement psn = cn.prepareStatement(Query);
+        psn.setString(1,LastName);
+        psn.setString(2,FirstName);
+        int  count = psn.executeUpdate();
+        System.out.println(count+" rows Effected");
+
+        sn.close();
+        cn.close();
+
+
+
+
     }
 }
